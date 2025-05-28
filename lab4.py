7856af26f7e21fc6400a92bc53c3ae34e3b9a847
cucainhoo <thanhthuvu0708@gmail.com>
2025-05-28 09:13:11 +0700

    Add files via upload

---
+++ lab4.py
@@ -0,0 +1,21 @@
+import streamlit as st
+# Đọc file
+st.title("Đọc file")
+file = 'cucainho.txt'
+with open(file=file, mode='r', encoding='utf-8') as f:
+    noi_dung = f.read()
+st.write(noi_dung)
+# Đếm từ
+st.title("Đếm từ")
+dem_tu = {}
+for tu in noi_dung.split():
+    dem_tu[tu] = dem_tu.get(tu, 0) + 1
+st.write("Số lần xuất hiện:\n", dem_tu)
+st.write("Tổng số từ: ",len(noi_dung.split()))
+# Thay thế
+st.title("Thay thế từ trong chuỗi")
+tu_can_thay = st.text_input("Nhập từ cần thay thế:\n")
+tu_moi = st.text_input("Nhập từ thay thế:\n")
+if tu_can_thay and tu_moi:
+    noi_dung_moi = noi_dung.replace(tu_can_thay, tu_moi)
+    st.write("Nội dung sau khi thay thế:\n", noi_dung_moi)
